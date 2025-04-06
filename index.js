acf39d19343c43cfbb7676f42c0ed02eddb48d63
Thavindu-Vishwa <thavinduwebsite@gmail.com>
2025-04-06 17:33:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// var x = document.getElementById('btn');
+function btnjs(){
+    
+    let FullName = document.getElementById('FullName').value;
+    let age = document.getElementById('age').value;
+    let Facility = document.getElementById('Facility').value;
+    let Department = document.getElementById('Department').value;
+
+        console.log4(FullName);
+    // console.log(FullName);
+    // console.log(age);
+    // console.log(Facility);
+    // console.log(Department);
+    // console.log();
+    
+    
+} 
+// alert('aa')
